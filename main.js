40cbc9eb778002df758dadc05098385d9e8caa2d
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-06-01 15:02:51 -0500

    Styled the board and added function to reorder it correctly

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+const order = [8, 3, 4, 1, 5, 9, 6, 7, 2];
 
 const createBoard = () => {
   const board = document.querySelector("#board");
@@ -11,4 +11,16 @@ const createBoard = () => {
 
 }
 
+const reorderBoard = () => {
+  let orderCounter = 1;
+  for (let i = 1; i <= 9; i++) {
+    let currentBoxID = order[i-1];
+    // console.log(document.querySelector("#board").innerHTML);
+    let currentBox = document.getElementById(currentBoxID);
+    let newBoxOrder = "" + i;
+    currentBox.style.order = newBoxOrder;
+  }
+}
+
 createBoard();
+reorderBoard();
